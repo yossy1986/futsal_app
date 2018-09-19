71215bb1f6739646c4a7ccb9fcd59a65a51155cb
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-09-19 01:35:36 +0000

    0919

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :courts
   resources :posts
   
-  get '/' => 'home#top'
+  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
