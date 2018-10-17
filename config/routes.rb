d3ec97c4fdb59faa065c77f6eb3b480ece0932a6
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-10-17 03:18:05 +0000

    1017

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-
+  root 'home#top'
 
   get 'apply_matchs/new' => 'apply_matchs#new'
   post 'apply_matchs/:id/create' => 'apply_matchs#create'
   patch 'apply_matchs/:id/update' => 'apply_matchs#update'
   delete 'apply_matchs/destroy' => 'apply_matchs#destroy'
+  post 'apply_matchs/:id/remove' => 'apply_matchs#remove'
+  
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  post 'apply_matchs/:id/remove' => 'apply_matchs#remove'
+  
+  get 'terms' => 'home#terms'
+  get 'privacy' => 'home#privacy'
+  get 'contact' => 'home#contact'
+  get 'advertising' => 'home#advertising'
+  
   resources :matchs do
     collection do
     get 'get_facilities'
@@ -24,10 +31,6 @@ Rails.application.routes.draw do
   resources :rooms
   resources :messages
   
-  get 'terms' => 'home#terms'
-  get 'privacy' => 'home#privacy'
-  get 'contact' => 'home#contact'
-  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
