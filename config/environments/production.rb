b606e5400b497d2aa13a3191bad7069bf083d01a
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-10-20 04:56:13 +0000

    1020

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = {  host: 'https://quiet-inlet-54819.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
+  host ='https://quiet-inlet-54819.herokuapp.com'
+  config.action_mailer.default_url_options = {  host: host }
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
