e068e2a5126faa22ff536ebf712d2a944902f574
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-10-20 05:05:44 +0000

    10201

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.raise_delivery_errors = true
-  host ='https://quiet-inlet-54819.herokuapp.com'
+  host ='https://quiet-inlet-54819.herokuapp.com/'
   config.action_mailer.default_url_options = {  host: host }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
