5fa333d2b352a3c494a8f30ab2e957d09d79bb55
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-10-24 14:51:51 +0000

    10246

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'yossy1986512@gmail.com'
+  default to: 'yossy1986512@yahoo.co.jp'
   layout 'mailer'
 end
--- test/mailers/previews/inquiry_mailer_preview.rb
+++ test/mailers/previews/inquiry_mailer_preview.rb
@@ -1,7 +1,5 @@
-# Preview all emails at http://localhost:3000/rails/mailers/inquiry_mailer
 class InquiryMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/inquiry_mailer/inquiry_email
   def inquiry_email
     InquiryMailer.inquiry_email
   end
