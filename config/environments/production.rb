ee2b9f92c6e4d080dcb4227ecb9e1059149b2c84
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-10-24 14:31:02 +0000

    10244

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,15 +92,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-
+mail = ENV['MAIL_ADDRESS']
+pass = ENV['MAIL_PASSWORD']
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
   domain:               'herokuapp.com',
-  user_name:            ENV['MAIL_USER_NAME'],
-  password:             ENV['MAIL_PASSWORD'],
+  user_name:            mail,
+  password:             pass,
   authentication:       'plain',
   enable_starttls_auto:  true
 }
