d04034a4acce2bf848e40cdf6ebdab1058e49b49
EC2 Default User <ec2-user@ip-172-31-0-119.us-east-2.compute.internal>
2018-10-24 16:12:37 +0000

    10248

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                  587,
   domain:               'gmail.com',
-  user_name:            ENV['MAIL_USER_NAME'],
-  password:             ENV['MAIL_PASSWORD'],
+  user_name:            'yossy1986512@gmail.com',
+  password:             'yazreteiiziqzpqs',
   authentication:       'plain',
   enable_starttls_auto:  true,
 }
